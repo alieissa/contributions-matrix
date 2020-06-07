dfbef1c8d1f3929d644cc41fd7d8cc881e1bdc05
ali eissa <aeissa.o@gmail.com>
2020-06-07 17:50:13 -0400

    Replace canvas with svg. Draw cells corresponding to days in the matrix.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Size of cell representing day (12) and padding around around it (2)
+const cellSize = 14
 // Query to get the last 53 weeks of contributions
 const contributionQuery = `{
   user(login: "alieissa") {
@@ -33,7 +35,26 @@ function getContributions() {
 }
 
 getContributions()
-  .then((data) => console.log('data', data))
-  .catch((err) => console.log('Unable to get contributions', err))
+  .then((data) => {
+    // Each week is drawn as a column
+    const weeks = d3
+      .select('svg')
+      .selectAll('g')
+      .data(data)
+      .enter()
+      .append('g')
+      .attr('transform', (_, i) => `translate(${cellSize * i}, 0)`)
 
-const ctx = d3.select('canvas').node().getContext('2d')
+    // Draw each day in week for all weeks
+    weeks.each((d, i, nodes) => {
+      d3.select(nodes[i])
+        .selectAll('rect')
+        .data(d.contributionDays)
+        .enter()
+        .append('rect')
+        .attr('x', 0)
+        .attr('y', (d) => cellSize * d.weekday)
+        .attr('fill', (d) => d.color)
+    })
+  })
+  .catch((err) => console.log('Unable to get contributions', err))
