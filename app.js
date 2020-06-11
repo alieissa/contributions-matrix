261f1c89776a33fa805972e63e57a4347462f639
ali eissa <aeissa.o@gmail.com>
2020-06-10 23:07:40 -0400

    Add basic tooltip to cells. Still a bit shaky.

--- app.js
+++ app.js
@@ -34,8 +34,20 @@ function getContributions() {
     )
 }
 
+function getTooltipContent(datum) {
+  if (datum.contributionCount === 0) {
+    return `No contributions on ${datum.date}`
+  } else if (datum.contributionCount === 1) {
+    return `1 contribution on ${datum.date}`
+  } else {
+    return `${datum.contributionCount} contributions on ${datum.date}`
+  }
+}
+
 getContributions()
   .then((data) => {
+    const tooltip = d3.select('.tooltip')
+
     // Each week is drawn as a column
     const weeks = d3
       .select('svg')
@@ -46,15 +58,26 @@ getContributions()
       .attr('transform', (_, i) => `translate(${cellSize * i}, 0)`)
 
     // Draw each day in week for all weeks
-    weeks.each((d, i, nodes) => {
+    weeks.each((week, i, nodes) => {
       d3.select(nodes[i])
         .selectAll('rect')
-        .data(d.contributionDays)
+        .data(week.contributionDays)
         .enter()
         .append('rect')
         .attr('x', 0)
         .attr('y', (d) => cellSize * d.weekday)
         .attr('fill', (d) => d.color)
+        .on('mouseover', (d) => {
+          tooltip.transition().duration(200).style('opacity', 0.9)
+          tooltip
+            .style('left', d3.event.pageX - 97.5 + 'px')
+            .style('top', d3.event.pageY - 34 + 'px')
+            .selectAll('.content')
+            .html(getTooltipContent(d))
+        })
+        .on('mouseout', (d) => {
+          tooltip.transition().duration(500).style('opacity', 0)
+        })
     })
   })
   .catch((err) => console.log('Unable to get contributions', err))
