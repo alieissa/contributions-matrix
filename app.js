6660cc0d7ef1091604946f47d1204b2ce044f84f
ali eissa <aeissa.o@gmail.com>
2020-06-13 01:31:47 -0400

    Fix tooltip flicker problem and positioning. Clean up.

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// TODO: Format date
+// TODO: Fix Tooltip styles, i.e. background, font color and padding
+// TODO: Add days and months axes
+// TODO: Fix table styles
+// TODO: Add contributions legend
+
 // Size of cell representing day (12) and padding around around it (2)
 const cellSize = 14
 // Query to get the last 53 weeks of contributions
@@ -18,8 +24,8 @@ const contributionQuery = `{
   }
 }`
 
-function getContributions() {
-  return fetch('https://api.github.com/graphql', {
+const getContributions = () =>
+  fetch('https://api.github.com/graphql', {
     method: 'POST',
     headers: {
       Authorization: `bearer ${token}`, // API token defined in config.js
@@ -32,8 +38,8 @@ function getContributions() {
       (body) =>
         body.data.user.contributionsCollection.contributionCalendar.weeks
     )
-}
 
+// Return information to be shown in tooltip
 function getTooltipContent(datum) {
   if (datum.contributionCount === 0) {
     return `No contributions on ${datum.date}`
@@ -42,6 +48,23 @@ function getTooltipContent(datum) {
   } else {
     return `${datum.contributionCount} contributions on ${datum.date}`
   }
+}
+
+// Gets the position of the tooltip so that tooltip is horizontally centered to
+// anchor rectangle
+const getTooltipLeftPosition = (d3Node) => {
+  const tooltipWidth = 210
+  const { x } = d3.select(d3Node).node().getBoundingClientRect()
+  return x - tooltipWidth / 2 + 'px'
+}
+
+// Gets the position of the tooltip so that tooltip is horizontally centered to
+// its anchor rectangle
+const getTooltipTopPosition = (d3Node) => {
+  const tooltipHeight = 28
+  const tooltipArrowHeight = 5
+  const { y } = d3.select(d3Node).node().getBoundingClientRect()
+  return y - 28 - 5 + 'px'
 }
 
 getContributions()
@@ -58,8 +81,8 @@ getContributions()
       .attr('transform', (_, i) => `translate(${cellSize * i}, 0)`)
 
     // Draw each day in week for all weeks
-    weeks.each((week, i, nodes) => {
-      d3.select(nodes[i])
+    weeks.each((week, i, weekNodes) => {
+      d3.select(weekNodes[i])
         .selectAll('rect')
         .data(week.contributionDays)
         .enter()
@@ -67,16 +90,25 @@ getContributions()
         .attr('x', 0)
         .attr('y', (d) => cellSize * d.weekday)
         .attr('fill', (d) => d.color)
-        .on('mouseover', (d) => {
-          tooltip.transition().duration(200).style('opacity', 0.9)
+        .on('mouseover', (d, j, dayNodes) => {
+          /**
+           * Cannot chain all methods together like
+           *    tooltip
+           *      .transitions
+           *      .duration(200)
+           *      .style('opacity', 0.8)
+           *      .style('left', :left)
+           * Error transition not a function error thrown
+           */
+          tooltip.transition().duration(200).style('opacity', 0.8)
           tooltip
-            .style('left', d3.event.pageX - 97.5 + 'px')
-            .style('top', d3.event.pageY - 34 + 'px')
-            .selectAll('.content')
+            .style('left', getTooltipLeftPosition(dayNodes[j]))
+            .style('top', getTooltipTopPosition(dayNodes[j]))
+            .selectAll('.info-container')
             .html(getTooltipContent(d))
         })
         .on('mouseout', (d) => {
-          tooltip.transition().duration(500).style('opacity', 0)
+          tooltip.transition().duration(200).style('opacity', 0)
         })
     })
   })
